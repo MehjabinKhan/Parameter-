2260f8794152f62630cce4388f8db36785020040
MehjabinKhan <mehjabinkhan2004@gmail.com>
2024-04-03 21:01:16 +0500

    Parameter

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+// Parameter
+function finalResult(totalMarks, obtainMarks) {
+    return obtainMarks / totalMarks * 100;
+}
+let marks = finalResult(550, 400);
+console.log(marks);
+function sum(num, num2) {
+    return num + num2;
+}
+let totalSum = sum(111, 789);
+console.log(totalSum);
---
+++ main.ts
@@ -0,0 +1,13 @@
+// Parameter
+function finalResult(totalMarks: number , obtainMarks: number): number {
+    return obtainMarks / totalMarks * 100 ;
+}
+let marks: number = finalResult(550 , 400)
+console.log(marks);
+
+function sum(num: number , num2: number): number {
+    return num + num2 ;
+}
+let totalSum: number = sum(111 , 789)
+console.log(totalSum);
+
